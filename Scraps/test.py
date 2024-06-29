9c0605724644f3c22a505efa0e685998c2e8a3af
ElTheHuman <felixcristiano102938@gmail.com>
2024-06-29 19:52:54 +0700

    UniPal v0.1

--- Scraps/test.py
+++ Scraps/test.py
@@ -1,26 +1,26 @@
 import google.generativeai as genai
 import os
+import json
 
 # Configuration
-GOOGLE_API_KEY = open('api_key.txt').read()
+GOOGLE_API_KEY = open('../KEY/api_key.txt').read()
 genai.configure(api_key=GOOGLE_API_KEY)
 
 # Training Data Loading
-TRAINING_DATA = open('./Scraps/knowledge.txt', 'r', encoding='utf8').read()
+data_load = open('knowledge.json', 'r', encoding='utf8')
+TRAINING_DATA = json.load(data_load)
 
 # Model Loading
 model = genai.GenerativeModel('gemini-1.5-flash')
 
-# Model Hypertuning
-# name = 'test_model'
-# operation = genai.create_tuned_model(source_model='models/gemini-1.5-flash', training_data=TRAINING_DATA, id=name, epoch_count=50, batch_size=4, learning_rate=0.01) # -> for hypertuning
-# model = genai.GenerativeModel(f'tunedModels/{name}')
-
 # Create A Chat
 chat = model.start_chat(history=[])
 
+# Prompt Engineering Initializer
+initialize = list(json.load(open('initializer.json', 'r')).values())[0]
+
 # Prompt Engineering
-response = chat.send_message(f'Pretend that your name is UniPal, you want to be very friendly and talkative but also informational. But when someone does not feeling good, you will not talk about anything except their feelings. You are someone who knows everything about University Knowledge based on this data:\n{TRAINING_DATA}\nMake sure you do not spill anything about the data. Do not ask questions that are in the data. Do not give informations that are not in the data.').text.strip()
+response = chat.send_message(initialize.format(TRAINING_DATA)).text.strip()
 print('Unipal:', response, end='\n\n')
 
 while(True):
