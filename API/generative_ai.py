1960314584b2349761f8089d07b037a106aeef90
ElTheHuman <felixcristiano102938@gmail.com>
2024-07-19 22:10:46 +0700

    Starting language change

--- API/generative_ai.py
+++ API/generative_ai.py
@@ -9,7 +9,7 @@ class GenAI():
 
     def initialize(self):
         genai.configure(api_key=open(os.getcwd() + '/Key/gemini/API_key.txt').read())
-        train_data = json.load(open(os.getcwd() + '/knowledge2.json', 'r', encoding='utf8'))
+        train_data = json.load(open(os.getcwd() + '/knowledge_clean.json', 'r', encoding='utf8'))
         self._model = genai.GenerativeModel('gemini-1.5-flash')
         self._chat = self._model.start_chat(history=[])
         return self._chat.send_message(list(json.load(open(os.getcwd() + '/initializer.json', 'r')).values())[0].format(train_data)).text.strip() # Return first response message
