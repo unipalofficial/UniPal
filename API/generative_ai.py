61bd3afa0a558fb75b1a515748665f546ab23ed9
ElTheHuman <felixcristiano102938@gmail.com>
2024-07-24 20:18:35 +0700

    Added new knowledge

--- API/generative_ai.py
+++ API/generative_ai.py
@@ -9,7 +9,13 @@ class GenAI():
 
     def initialize(self):
         genai.configure(api_key=open(os.getcwd() + '/Key/gemini/API_key.txt').read())
-        train_data = json.load(open(os.getcwd() + '/knowledge_clean.json', 'r', encoding='utf8'))
+        get_data = json.load(open(os.getcwd() + '/knowledge_clean.json', 'r', encoding='utf8'))
+        train_data = []
+        train_data.append(get_data[:int(.5*len(get_data))])
+        train_data.append(get_data[int(.5*len(get_data)):]) # Initialize train data
         self._model = genai.GenerativeModel('gemini-1.5-flash')
         self._chat = self._model.start_chat(history=[])
-        return self._chat.send_message(list(json.load(open(os.getcwd() + '/initializer.json', 'r')).values())[0].format(train_data)).text.strip() # Return first response message
+        for i in range(2):
+            data = json.load(open(os.getcwd() + '/initializer.json', 'r'))[i]
+            for key in data.keys():
+                print(self._chat.send_message(data[key].format(train_data[i])).text.strip())
